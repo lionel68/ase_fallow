9f4b2fe386272e08c94441a1d0671c982bbef381
lionel68 <lionel.hertzog@ugent.be>
2021-10-05 16:09:17 +0200

    remove sql files

--- script/sqlscript/sql_atkis_agri.sql
+++
@@ -1,7 +0,0 @@
-select b.routcode, st_union(st_intersection(a.geom, st_buffer(st_transform(st_centroid(b.geom), 25832), 1000))) as agrigeom
-from dlm1.dlm18_aaa_utm32_gf_v2_s a
-join public.mhb_plot b
-on st_intersects(a.geom, st_buffer(st_transform(st_centroid(b.geom), 25832), 1000))
-where a.oba_18 in (4101, 4102, 4103, 4104, 4109)
-group by b.routcode
-order by b.routcode
--- script/sqlscript/sql_atkis_edge.sql
+++
@@ -1,7 +0,0 @@
-create table mhb_1kmbuffer_edge as
-select a.routcode, sum(st_length(st_intersection(a.geom, b.agrigeom))) as edge_m
-from public.mhb_1kmbuffer_forest a
-join public.mhb_1kmbuffer_agri b
-on a.routcode = b.routcode
-group by a.routcode
-order by a.routcode
