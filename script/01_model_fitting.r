6de38131f4f45627575fb7606720987f4a4322aa
Lionel Hertzog <lionel.hertzog@thuenen.de>
2022-07-26 09:10:00 +0200

    update for revision

--- script/01_model_fitting.r
+++ script/01_model_fitting.r
@@ -1,26 +1,20 @@
 ##############
-# R script to fit the models
-# and run model checks
+# R script to fit the species richness
+# and group-level abundance models
 #######################
 
 
 # set wd
-setwd(dirname(rstudioapi::getSourceEditorContext()$path))
+setwd("~/PostDoc_Thunen/ASE-stuff/")
+source("script/rscript/XX_helper_function.R")
 
 # load libraries
-library(rstan)
-options(mc.cores = parallel::detectCores())
-Sys.setenv(LOCAL_CPPFLAGS = '-march=corei7 -mtune=corei7') # only on win machine
-rstan_options(auto_write = TRUE)
 library(tidyverse)
-# library(gridExtra)
-library(sf)
 library(DHARMa)
 library(brms)
 
 # load data
-bird <- read.csv("../data/bird_data.csv")
-district <- read.csv("../data/district_fallow.csv")
+bird <- read.csv("data/preprocessed/bird_fallow_v14.csv")
 
 # some transformation / scaling of covariates
 bird$fallow_sqrt <- sqrt(bird$fallow_per)
@@ -29,172 +23,21 @@ bird$edge_std <- scale(bird$edge_mha)
 bird$agri_std <- scale(bird$agriculture)
 
 
+# fit the models and save them in the subfolder model_output/stanmodel
+for(yr in c(2007, 2010, 2016)){
+  subdat <- subset(bird, year == yr)
 
-# priors for the species richness model
-bprior_rich <- c(prior(normal(0, 2.5), class = Intercept),
-                prior(normal(0, 2.5), class = b),
-                prior(student_t(3, 0, 2.5), class = sd),
-                prior(student_t(3, 0, 2.5), class = sigma))
-
-# fitting the species richness model
-# note that due to z ~ N(mu, sd) is the same as 
-# z = mu + sd * d where d ~ N(0, 1)
-# we can write the hierarchical model with group-level
-# covariates in brms
-m_rich <- brm(bf(rich | trunc(lb = 0) ~ agri_std + (edge_std + I(edge_std ^ 2)) * fallow_std + 
-                   (1 | NUTS_code / NUTS_year) + (0 + edge_std | NUTS_code / NUTS_year) +
-                   (0 + I(edge_std ^ 2) | NUTS_code /NUTS_year)),
-              data = bird, family = "normal", prior = bprior_rich,
-              control = list(adapt_delta = 0.9, max_treedepth = 25))
-
-# priors for the aubundance models
-bprior <- c(prior(normal(0, 2.5), class = Intercept),
-            prior(normal(0, 2.5), class = b),
-            prior(student_t(3, 0, 2.5), class = sd),
-            prior(gamma(0.01, 0.01), class = shape))
-
-# fitting the three abundance models
-m_edge <- brm(bf(edge ~ agri_std + edge_std * fallow_std + (1 | NUTS_code / NUTS_year) +
-                   (0 + edge_std | NUTS_code / NUTS_year),
-                 zi ~ agri_std + edge_std),
-              data = bird, family = "zero_inflated_negbinomial",
-              prior = bprior,
-              control = list(adapt_delta = 0.9, max_treedepth = 25))
-
-m_field <- brm(bf(field ~ agri_std + edge_std * fallow_std + (1 | NUTS_code / NUTS_year) +
-                    (0 + edge_std | NUTS_code / NUTS_year),
-                  zi ~ agri_std + edge_std),
-              data = bird, family = "zero_inflated_negbinomial", 
-              control = list(max_treedepth = 25, adapt_delta = 0.9))
-
-m_low <- brm(bf(low ~ agri_std + edge_std * fallow_std + (1 | NUTS_code / NUTS_year) +
-                  (0 + edge_std | NUTS_code / NUTS_year),
-                  zi ~ agri_std + edge_std),
-               data = bird, family = "zero_inflated_negbinomial", 
-               control = list(max_treedepth = 25, adapt_delta = 0.9))
-
-# save the models
-m_list <- list(edge = m_edge, field = m_field, low = m_low, rich = m_rich)
-saveRDS(m_list, "model_output/fitted_model.rds")
-
-
-## model checks
-
-### first posterior predictive distributions
-pp_field <- pp_check(m_list$field, nsamples = 50) +
-  labs(title = "Field-breeders") +
-  scale_x_continuous(trans = "sqrt")
-pp_edge <- pp_check(m_list$edge, nsamples = 50) +
-  labs(title = "Edge-breeders") +
-  scale_x_continuous(trans = "sqrt")
-pp_low <- pp_check(m_list$low, nsamples = 50) +
-  labs(title = "Foraging visitors") +
-  scale_x_continuous(trans = "sqrt")
-pp_rich <- pp_check(m_list$rich, nsamples = 50) +
-  labs(title = "Species richness") +
-  scale_x_continuous(trans = "sqrt")
-
-gg_pp <- ggpubr::ggarrange(pp_rich, pp_field, pp_edge, pp_low, nrow = 2, ncol = 2, common.legend = TRUE, legend = "bottom")
-ggsave(filename = "figures/pp_checks.png", gg_pp)
-
-### then DHARMa checks
-dd_edge <- createDHARMa(t(posterior_predict(m_list$edge)),
-                        bird$edge, integerResponse = TRUE)
-dd_field <- createDHARMa(t(posterior_predict(m_list$field)),
-                         bird$field, integerResponse = TRUE)
-dd_low <- createDHARMa(t(posterior_predict(m_list$low)),
-                       bird$low, integerResponse = TRUE)
-dd_rich <- createDHARMa(t(posterior_predict(m_list$rich)),
-                       bird$rich, integerResponse = TRUE)
-
-png("figures/dharma_edge.png")
-plot(dd_edge)
-dev.off()
-
-png("figures/dharma_field.png")
-plot(dd_field)
-dev.off()
-
-png("figures/dharma_low.png")
-plot(dd_low)
-dev.off()
-
-png("figures/dharma_rich.png")
-plot(dd_rich)
-dev.off()
-
-### the spatial autocorr checks
-# ! this code is not reproducible due to data policy on the spatial location of the plots !
-
-mhb <- st_read("data/geodata/MhB_plots_S2637.shp")
-mhb %>%
-  rename(routcode = ROUTCODE) %>%
-  st_drop_geometry() %>%
-  select(routcode, X_COORD, Y_COORD) %>%
-  right_join(bird) -> bird2
-
-## an helper function
-spat_autocorr <- function(model, data, colname = "field"){
-  spp <- data.frame(routcode = character(0), type = character(0),
-                    year = numeric(0), x = numeric(0),
-                    y = numeric(0) , res = numeric(0))
-  aa <- data.frame(type = character(0), year = numeric(0),
-                   moran = numeric(0), p_val = numeric(0))
-  for(i in 1:3){
-    year <- c(2007, 2010, 2016)[i]
-    tmp <- createDHARMa(t(posterior_predict(model))[data$year==year,],
-                                 data[,colname][data$year==year], 
-                                 integerResponse = TRUE)
-    
-    autocorr <- testSpatialAutocorrelation(tmp, x = data$X_COORD[data$year == year],
-                                           y = data$Y_COORD[data$year == year],
-                                           plot = FALSE)
-    
-    aa <- rbind(aa, data.frame(type = colname, year = year, 
-                               moran = autocorr$statistic["observed"],
-                               p_val = autocorr$p.value))
-    
-    spp <- rbind(spp, data.frame(routcode = data$routcode[data$year==year],
-                                 type = colname,
-                                 year = year,
-                                 x = data$X_COORD[data$year==year],
-                                 y = data$Y_COORD[data$year==year],
-                                 res = tmp$scaledResiduals))
-    
-  }
+  # rich model
+  m_rich <- fit_stan_rich(subdat, "rich", "model_output/stanmodel/", paste0("rich_tn", yr))
+  # edge model
+  m_edge <- fit_stan_abund(subdat, "edge", "model_output/stanmodel/", paste0("edge_zinb", yr))
+  extract_beta(m_edge, "model_beta_grp", paste0("edge_", yr))
+  # field model
+  m_field <- fit_stan_abund(subdat, "field", "model_output/stanmodel/", paste0("field_zinb", yr))
+  extract_beta(m_field, "model_beta_grp", paste0("field_", yr))
+  # low model
+  m_low <- fit_stan_abund(subdat, "low", "model_output/stanmodel/", paste0("low_zinb", yr))
+  extract_beta(m_low, "model_beta_grp", paste0("low_", yr))
   
-  return(list(auto = aa, df = spp))
+  print(yr)
 }
-
-field_auto <- spat_autocorr(m_list$field, bird2, colname = "field")
-edge_auto <- spat_autocorr(m_list$edge, bird2, colname = "edge")
-low_auto <- spat_autocorr(m_list$low, bird2, colname = "low")
-rich_auto <- spat_autocorr(m_list$rich, bird2, colname = "rich")
-
-# put things together for plotting
-dff <- rbind(field_auto$df, edge_auto$df, low_auto$df, rich_auto$df)
-aaa <- rbind(field_auto$auto, edge_auto$auto, low_auto$auto, rich_auto$auto)
-dff <- left_join(dff, aaa, by = c("type", "year"))
-dff$type <- factor(dff$type, 
-                   labels = c("Field-breeders",
-                              "Edge-breeders",
-                              "Foraging visitors",
-                              "Species richness"))
-
-dff$code <- paste0(dff$type, "\nyear: ", dff$year,
-                   "\nMoran's I: ", round(dff$moran, 2),
-                   " , p-value: ", round (dff$p_val, 0))
-
-gg_sp <- ggplot(dff, aes(x=x, y=y, color = res)) +
-  geom_point() +
-  facet_wrap(~ code, nrow = 4) +
-  scale_color_gradient2(low = "red", mid="white", 
-                        high="blue", midpoint = 0.5) +
-  theme(axis.text = element_blank(),
-        axis.ticks = element_blank(),
-        panel.background = element_blank(),
-        text = element_text(size = 15)) +
-  labs(x="", y="")
-
-ggsave("figures/spat_autocorr.png", gg_sp,
-       width = 12, height = 16)
--- script/03_model_fitting_species_lvl.r
+++ script/03_model_fitting_species_lvl.r
@@ -1,109 +1,48 @@
-# analysis script
-setwd("PostDoc_Thunen/ASE-stuff/")
+############
+# R script to fit species-level
+# abundance models, run model checks
+# and extract slope coefficients
+#############################################
 
-# the path to save the outputs, must have sub-folder: model, dharma_fig and fallow_fig
-output_path <- "/data/monvia_birds/ase_analysis"
+# set wd
+setwd("~/Dokumente/Bird-stuff/monvia_voegel/ro2_ase_fallow/")
+source("script/rscript/XX_helper_function.R")
 
 # load libraries
 library(brms)
 options(mc.cores = parallel::detectCores())
 library(tidyverse)
-# library(gridExtra)
-library(sf)
-library(DHARMa)
 
 # load data
-bird <- read.csv("data/preprocessed/bird_fallow_species.csv")
-#bird$land <- substr(bird$routcode, 1, 2)
+bird <- read.csv("data/preprocessed/bird_fallow_species_2.csv")
 
 # some transformation
-bird$year_cat <- factor(bird$year)
-bird$fallow_sqrt <- sqrt(bird$fallow)
+bird$fallow_sqrt <- sqrt(bird$fallow_per)
 bird$fallow_std <- scale(bird$fallow_sqrt)
-bird$edge_std <- scale(bird$edge_m)
-bird$urban_std <- scale(bird$urban)
-bird$forest_std <- scale(bird$forest)
-bird$bkr <- factor(bird$bkr)
+bird$edge_std <- scale(bird$edge_mha)
+bird$agri_std <- scale(bird$agriculture)
 
-# helper function to grab the warnings
-tryCatch.W.E <- function(expr)
-{
-  W <- NULL
-  w.handler <- function(w){ # warning handler
-    W <<- w
-    invokeRestart("muffleWarning")
-  }
-  list(value = withCallingHandlers(tryCatch(expr, error = function(e) e),
-                                   warning = w.handler),
-       warning = W)
-}
+# a species x year data.frame
+species_year <- expand.grid(species = unique(bird$species),
+                            year = c(2007, 2010, 2016))
 
-# helper function and data for the fallow plot
-pred_fallow <- expand.grid(fallow_std = seq(min(bird$fallow_std),
-                                            max(bird$fallow_std),
-                                            length.out = 10),
-                           edge_std = quantile(bird$edge_std, 
-                                               probs = c(0.1, 0.5, 0.9)),
-                           urban_std = 0, forest_std = 0, year_cat = unique(bird$year_cat))
-# helper function
-plot_1 <- function(model, newdata, species){
-  # expected model values
-  predd <- posterior_epred(model, newdata, re.form = NA)
-  # average over the years
-  predd <- (predd[,1:30] + predd[,31:60] + predd[,61:90]) / 3
-  # compute quantiles
-  pred_df <- cbind(newdata, posterior_summary(predd))
-  pred_df$edge_cat <- factor(rep(c("low", "middle", "high"), each = 10),
-                             levels = c("low", "middle", "high"))
-  pred_df$fallow_ha <- (pred_df$fallow_std * sd(bird$fallow_sqrt) + mean(bird$fallow_sqrt)) ** 2
+# loop through this
+for(i in 1:nrow(species_year)){
+  id <- paste(species_year[i, "species"],
+              species_year[i, "year"], sep = "_")
+  # subset the data
+  data <- subset(bird, species == species_year[i, "species"] & 
+                   year == species_year[i, "year"])
+  
+  # fit the model
+  model <- fit_stan_abund(data, "Abundance", "model_output/stanmodel/", file_id = id)
+  
+  # perform model checks
+  checks <- stan_check(model, data, "Abundance", write_file = TRUE, file_id = id)
+  
+  # extract beta parameters of fallow effect and interaction
+  betaa <- extract_beta(model, file_id = id)
 
-  gg_plot11 <- ggplot(pred_df, aes(x=fallow_ha, y = Estimate, ymin = Q2.5, ymax = Q97.5)) +
-    geom_ribbon(aes(fill=edge_cat), color = NA, alpha = 0.15) +
-    geom_line(aes(color=edge_cat)) +
-    guides(fill = guide_legend(title = "Structural complexity\nof the landscape : "),
-           color = guide_legend(title = "Structural complexity\nof the landscape : ")) +
-    labs(x = "Area of fallow in 1km buffer (ha)",
-         y = "Predicted abundance (with 95% CrI)",
-         title = species) +
-    theme(panel.background = element_blank(),
-          axis.line = element_line())
+  print(paste0(id, " is done!\n"))
   
-  ggsave(paste0(output_path, "/fallow_fig/", species, ".png"), gg_plot11)
-}
-
-# helper function to fit the model and generate various outputs
-fit_brms <- function(data, species, output_path, ...){
-  
-  print(paste0("Starting fitting for : ", species))
-  # the model
-  m <- tryCatch.W.E(brm(bf(Abundance ~ year_cat + edge_std * fallow_std + (1 | bkr)),
-           data = subset(data, species == sp), 
-           family = "zero_inflated_negbinomial"))
-  
-  # save the model with warnings / errors
-  saveRDS(m, file = paste0(output_path, "/model/", species, ".rds"))
-  
-  # make a DHARMa object
-  if(!is.null(m$warning)){
-    dd <- createDHARMa(t(posterior_predict(m$value)),
-                       bird$Abundance[bird$Artname == species],
-                       integerResponse = TRUE)
-    
-    # save the DHARMa plot
-    png(paste0(output_path, "/dharma_fig/", species, ".png"))
-    plot(dd)
-    dev.off()
-  }
-  
-  # make the fallow prediction plot
-  if(!is.null(m$warning)){
-    plot_1(m$value, pred_fallow, species)
-  }
-  
-  return(paste0(species, " finished just fine!"))
-}
-
-# the actual fitting
-for(sp in unique(bird$Artname)){
-  fit_brms(bird, sp, output_path)
 }
